387ff464c514bb5023cc8a2cf3907784c4e64f08
Ahmed Hamuda <ahmed.hamuda@drei.com>
2017-12-20 10:43:20 +0100

    add ping to the server

--- app.js
+++ app.js
@@ -11,3 +11,4 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 });
 // Listen for messages from users 
 server.post('/api/bot/messages', connector.listen());
+server.get("/", () => { return "ok"});
