0f4fe009dc48c892bcd58d3bad8d5f0f55ed7a61
Ahmed Hamuda <ahmed.hamuda@drei.com>
2017-12-22 15:17:15 +0100

    Adding session management

--- app.js
+++ app.js
@@ -4,16 +4,26 @@ exports.__esModule = true;
 
 require('dotenv-extended').load();
 const restify = require("restify");
+const sessions = require("client-sessions");
 const connector = require("./bot/bot");
 const jiraOAuth = require("./jira_oauth");
 
 let server = restify.createServer();
+server.use(restify.bodyParser());
+server.use(sessions({
+    cookieName: "session",
+    secret: "GDSHR2rwaf32",
+    duration: 24 * 60 * 60 * 1000,
+    activeDuration: 1000 * 60 * 5
+}));
+
 server.listen(process.env.port || process.env.PORT || 3978, process.env.WEB_HOSTNAME, () => {
     console.log('listening to %s', server.url);
 });
 // Listen for messages from users 
 server.post('/api/bot/messages', connector.listen());
 server.get("/api/jira/callback", (req,res) => {
+    console.log(req.session);
     jiraOAuth.callback(req,res);
 });
 server.get("/api/jira/tokenRequest", (req,res) => {
